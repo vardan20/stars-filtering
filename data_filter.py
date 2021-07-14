b2ade7f690d84e833f4932b9373d837f2efb1778
Vardan <vardan.vardanyan.fd@gmail.com>
2021-07-14 15:40:30 +0400

    Read function is added
    Read function is added in data_filter module, which reads .tsv file, and returns dictionary

--- data_filter.py
+++ data_filter.py
@@ -1,7 +1,29 @@
 import logging
 import csv
 
+characteristics = [] #all characteristics of stars, such as RA, DEC, etc.
+
+def read(filename):
+    global characteristics
+    with open(filename) as fd:
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        next(rd)
+        characteristics = (next(rd))
+        dct = {}
+        for cur_char in characteristics:
+            dct[cur_char] = []
+        for row in rd:
+            char_id = 0
+            for cur_char in characteristics:
+                dct[cur_char].append(row[char_id])
+                char_id += 1
+
+    return dct
+
 
 class DataFilter:
     def run(self):
         print("Data Filter process is started")
+        table = read("337.all.tsv")
+        #print(table['ra_ep2000'][0:5])
+        #print(characteristics)
